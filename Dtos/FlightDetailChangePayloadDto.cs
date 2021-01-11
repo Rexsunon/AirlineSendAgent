5c7c246ec49345b2883fe7dce44f55fef26fad65
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-01-11 17:20:49 +0100

    Created NotificationMessage and FlightDetailChangePayload dto models

---
+++ Dtos/FlightDetailChangePayloadDto.cs
@@ -0,0 +1,13 @@
+namespace TravelAgentWeb.Dtos
+{
+    public class FlightDetailChangePayloadDto
+    {
+        public string WebhookURI { get; set; }
+        public string Publisher { get; set; }
+        public string Secret { get; set; }
+        public string FlightCode { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal NewPrice { get; set; }
+        public string WeebhookType { get; set; }
+    }
+}
