78146c153575055d8f1fb4bfd28552cd71b792bb
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-01-11 16:51:52 +0100

    Setup RabbitMQ and listen for event on the message bus

--- App/AppHost.cs
+++ App/AppHost.cs
@@ -1,10 +1,42 @@
+using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
 namespace AirlineSendAgent.App
 {
     public class AppHost : IAppHost
     {
         public void Run()
         {
-            throw new System.NotImplementedException();
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(
+                    queue: queueName,
+                    exchange: "trigger",
+                    routingKey: ""
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                Console.WriteLine("Listening on the message bus...");
+
+                consumer.Received += async (ModuleHandle, ea) => {
+                    Console.WriteLine("Event is triggered");
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.ReadLine();
+            }
         }
     }
 }
